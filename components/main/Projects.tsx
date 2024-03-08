87fc5716159003383919e6c2189a07d62d9482f9
omar Mezzour <mezzour.omar@outlook.com>
2024-03-08 14:23:44 +0100

    add files

---
+++ components/main/Projects.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/ansible-vagrant.webp"
+          title="ANSIBLE for DevOps"
+          description="This Ansible project is designed to orchestrate and deploy a multi-server environment consisting of web, database, and logging servers. Leveraging Vagrant for local testing and Ansible for configuration management, the project provides an organized structure, defined playbooks, and specific roles for each server, simplifying the deployment and maintenance of IT infrastructures."
+        />
+        <ProjectCard
+          src="/netw_scann.png"
+          title="Network Scanner with Python"
+          description="The Network Scanner project involves developing a utility to examine networks, detecting connected devices, open ports, and potential security risks. Leveraging Python libraries like Scapy or Nmap, this tool provides a straightforward solution for network administrators and security experts to conduct thorough network scans, generating detailed reports to proactively address vulnerabilities and enhance network security."
+        />
+        <ProjectCard
+          src="/Apache_HTTP_Server-Logo.wine.png"
+          title="Secure Apache with SELinux Integration"
+          description="The SELinux Integration with Apache project aims to enhance security by seamlessly integrating SELinux with the Apache web server. By tailoring SELinux policies for Apache, it restricts access to necessary resources, minimizing risks like SQL injection and cross-site scripting, thus strengthening the overall security of web servers."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ constants/index.ts
@@ -0,0 +1,260 @@
+export const Skill_data = [
+    {
+      skill_name: "Wireshark",
+      Image: "/wireshark-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "METASPLOITABLE",
+      Image: "/metasploit-framework-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "LINUX",
+      Image: "/linux-icon.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Maltego",
+      Image: "/maltego-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Ettercap",
+      Image: "/ettercap-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "SQLMap",
+      Image: "/sqlmap-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Netcat",
+      Image: "/netcat-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Nikto",
+      Image: "/nikto-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Hydra",
+      Image: "/hydra-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "John The Ripper",
+      Image: "/john-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Nmap",
+      Image: "/nmap-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Burp Suite",
+      Image: "/burpsuite-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Aircrack ng",
+      Image: "/aircrack-ng-logo.svg",
+      width: 60,
+      height: 60,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Mail",
+      src: "/gmail_icon.webp",
+      link: "mailto:mezzour.omar@outlook.com"
+    },
+    {
+      name: "LinkedIn",
+      src: "/linkedin-logo.webp",
+      link: "https://www.linkedin.com/in/omarmezzour/"
+    },
+    {
+      name: "GitHub",
+      src: "/gitwhite.png",
+      link: "https://github.com/omarmezzour"
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Node JS",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Python",
+      Image: "/python-logo.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java",
+      Image: "/java-icon.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Github",
+      Image: "/github-142-svgrepo-com.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "VS Code",
+      Image: "/vscode-icon.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Pycharm",
+      Image: "/PyCharm_Icon.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Scapy",
+      Image: "/scapy-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Bettercap",
+      Image: "/bettercap-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Dirb",
+      Image: "/dirb-logo.svg",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Docker",
+      Image: "/docker.webp",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/mysql.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Postger SQL",
+      Image: "/postger.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Auto-Spy",
+      Image: "/autopsy-logo.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "dSniff",
+      Image: "/dsniff-logo.svg",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Full_stack = [
+    {
+      skill_name: "Kali",
+      Image: "/kali-logo.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "RedHat",
+      Image: "/redhat-icon.svg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Parrot",
+      Image: "/parrot-logo.jpeg",
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "Fedora",
+      Image: "/fedora-icon.png",
+      width: 70,
+      height: 70,
+    },
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "Hack The Box",
+      Image: "/htb-logo.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  
+  
+  
+  
