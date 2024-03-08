63d85c8e4ca46221c9ab95fc5142230330aed7f2
omar Mezzour <mezzour.omar@outlook.com>
2024-03-08 14:39:57 +0100

    add files

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -25,7 +25,7 @@ const Encryption = () => {
       <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
           <Image
-            src="/LockTop.png"
+            src="./LockTop.png"
             alt="Lock top"
             width={50}
             height={50}
